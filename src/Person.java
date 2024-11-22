d51dc252b16e46a8a5360c2991bd9b80f0e199a5
Dominic <Dominicscott461@gmail.com>
2024-11-21 23:06:55 -0500

    Added start of Person Class

---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    private String Name;
+    private String Role;
+
+    public Person(String name, String role) {
+        this.Name = name;
+        this.Role = role;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void Set_Name(String name){
+        this.Name = name;
+
+    }
+
+}
